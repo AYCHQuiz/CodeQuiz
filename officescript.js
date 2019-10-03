02533ccc822e7a7f620badbe96a0d0c580da106e
Carina Ullman <carinalullman@gmail.com>
2019-10-03 12:51:32 -0600

    Questions have been created

---
+++ officescript.js
@@ -0,0 +1,23 @@
+
+//Psuedo code:
+// needs a call to action to "start quiz" (alert and button)
+
+var startQuiz = alert ("Please click start button to begin quiz");
+
+//need to build a high score page and a timer that starts at 75 seconds (increments, count++)
+
+//needs a onclick event to start the quiz (make a button also) and then the questions will appear
+//timer needs to start as soon as the question appears.  Make sure the timer is given a max amount of time for the user to click an answer and count downwards
+
+//start calculating the score incrementally when the user gets a right answer ; create an alert on the page that tells you if the answer is right or wrong
+//answering incorrectly results in a time penalty of 5 seconds from the total of seconds
+
+//make it responsive in html
+
+//when timer is done and/or all questions have been answered, show final score and create an input for the user to enter intials
+// then show final score and initials in "local storage"
+
+// store questions as an array of objects in a separate js file
+// so you will need to reference questions.js
+//length of array will determine the length of play: 15 seconds per question so if i have 5 questions then the time will be 75
+
